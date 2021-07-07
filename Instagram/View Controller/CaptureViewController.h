6ef3b35d3abbccb78c839cd8fffb6a955595cec4
Jose Serrano <jose.serrano22@upr.edu>
2021-07-07 00:18:28 -0400

    started with capture

---
+++ Instagram/View Controller/CaptureViewController.h
@@ -0,0 +1,16 @@
+//
+//  CaptureViewController.h
+//  Instagram
+//
+//  Created by jose1009 on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CaptureViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Instagram/View Controller/CaptureViewController.m
@@ -0,0 +1,31 @@
+//
+//  CaptureViewController.m
+//  Instagram
+//
+//  Created by jose1009 on 7/6/21.
+//
+
+#import "CaptureViewController.h"
+
+@interface CaptureViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@end
+
+@implementation CaptureViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
