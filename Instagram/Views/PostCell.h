4b4d8f648cc855bd1bd248ce7fcacad8ce6d38af
Jose Serrano <jose.serrano22@upr.edu>
2021-07-07 18:33:05 -0400

    created postcell

---
+++ Instagram/Views/PostCell.h
@@ -0,0 +1,25 @@
+//
+//  PostCell.h
+//  Instagram
+//
+//  Created by jose1009 on 7/7/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PostCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *profileImage;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timestampLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *postImage;
+@property (weak, nonatomic) IBOutlet UIButton *favoriteButton;
+@property (weak, nonatomic) IBOutlet UIButton *commentButton;
+@property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Instagram/Views/PostCell.m
@@ -0,0 +1,23 @@
+//
+//  PostCell.m
+//  Instagram
+//
+//  Created by jose1009 on 7/7/21.
+//
+
+#import "PostCell.h"
+
+@implementation PostCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
